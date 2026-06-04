ada7389144bd30f620170e0f4c336dd484238004
weiwei <63530758+CFIWEI2020@users.noreply.github.com>
2026-06-04 14:47:26 +0800

    Day13-Equipment-Dashboard-Visualization

---
+++ Day13-Equipment-Dashboard-Visualization.jl
@@ -0,0 +1,115 @@
+#讀資料 → 建立 status → 建立 status_count → 畫圖 → 存圖
+
+using CSV
+using DataFrames
+using Plots
+
+df = CSV.read(
+    "equipment_data.csv",
+    DataFrame
+)
+println(df)
+println(names(df))
+
+function check_status(temp,pressure)
+    if temp > 100 || pressure > 2.3
+        return "Critical"
+    elseif temp > 85 || pressure > 1.8
+        return "Warning"
+    else 
+        return "Normal"
+    end
+end
+
+df.status = [
+    check_status(row.temperature , row[:pressure])
+    for row in eachrow(df)
+]
+
+status_count = combine(
+    groupby(df, :status),
+    nrow => :count
+)
+
+println(df)
+println(status_count)
+
+
+p1 = histogram(
+    df.temperature,
+    bins = 5,
+    title = "Temperature Distribution"
+)
+
+savefig(p1, "temperature_distribution.png")
+
+p2 = histogram(
+    df.pressure,
+    bins = 5,
+    title = "Pressure Distribution"
+)
+savefig(p2, "pressure_distribution.png")
+
+p3 = bar(
+    status_count.status,
+    status_count.count,
+    title = "Equipment Status",
+    xlabel = "Status",
+    ylabel = "Count"
+)
+savefig(p3, "equipment_status.png")
+
+p4 = bar(
+    df.equipment_id,
+    df.temperature,
+    title = "Equipment Temperature",
+    xlabel = "Equipment ID",
+    ylabel = "Temperature"
+)
+savefig(p4, "equipment_temperature.png")
+
+p5 = bar(
+    df.equipment_id,
+    df.pressure,
+    title = "Equipment Pressure",
+    xlabel = "Equipment ID",
+    ylabel = "Pressure"
+)
+savefig(p5, "equipment_pressure.png")
+
+#=
+histogram(
+    df.pressure,
+    bins = 5,
+    title = "Pressure Distribution"
+)
+
+
+bar(
+    status_count.status,
+    status_count.count,
+    title = "Equipment Status",
+    xlabel = "Status",
+    ylabel = "Count"
+)
+
+println(df)
+println(names(df))
+
+bar(
+    df.equipment_id,
+    df.temperature,
+    title = "Equipment Temperature",
+    xlabel = "Equipment ID",
+    ylabel = "Temperature"
+)
+
+bar(
+    df.equipment_id,
+    df.pressure,
+    title = "Equipment Pressure",
+    xlabel = "Equipment ID",
+    ylabel = "Pressure"
+)
+=#
+
